cd7aff6b0b6b4b8cbd9bb629a2a05283a9256fc8
Ido Mandelman <idomand@gmail.com>
2023-09-04 20:39:20 +0200

    updating tests

--- src/examples/counter/counter.test.tsx
+++ src/examples/counter/counter.test.tsx
@@ -1,10 +1,18 @@
-import { screen, render } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+//@vitest-environment happy-dom
+import { screen, fireEvent } from '@testing-library/react';
 import Counter from '.';
 
-test.todo('it should render the component', () => {});
+import { render } from './test/utilities';
 
-test.todo(
-  'it should increment when the "Increment" button is pressed',
-  async () => {},
-);
+test('it should render the component', () => {
+  render(<Counter />);
+});
+
+test('it should increment when the "Increment" button is pressed', async () => {
+  const { user } = render(<Counter />);
+  const currentCount = screen.getByTestId('current-count');
+  expect(currentCount).toHaveTextContent('0');
+  const Button = screen.getByRole('button', { name: 'Increment' });
+  await user.click(Button);
+  expect(currentCount).toHaveTextContent('1');
+});
--- src/examples/counter/test/utilities.ts
+++ src/examples/counter/test/utilities.ts
@@ -1,5 +1,16 @@
-export {};
+export * from '@testing-library/react';
 
-/**
- * For a complete example, see: test/utilities.ts
- */
+import userEvent from '@testing-library/user-event';
+import {
+  screen,
+  render as renderComponent,
+  fireEvent,
+} from '@testing-library/react';
+import React from 'react';
+
+export const render = (
+  ui: React.ReactElement,
+  options?: Parameters<typeof renderComponent>[1],
+) => {
+  return { ...renderComponent(ui, options), user: userEvent.setup() };
+};
